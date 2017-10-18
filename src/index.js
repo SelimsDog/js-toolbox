1b0ea52ae6f8a4b53ee6d84f71cff4906ea57a75
Yavuz Selim Abidin <ysabidin@gmail.com>
2017-10-19 02:29:34 +0300

    readme added, selector

--- src/index.js
+++ src/index.js
@@ -3,10 +3,32 @@
  * if first parameter is undefined, it will return second parameter.
  * @param {*} value 
  * @param {*} def 
+ * @return {*}
  */
 function setDef(value, def) {
       return value !== undefined ? value : def;
 }
 
 
-export {setDef}
+/**
+ * select DOM elements. 
+ * If param starts with "#" it will select by ID and return DOM, if not found, it will return null
+ * If param starts with "." it will select by Class name and return Array
+ * @param {String} param 
+ * @return {Array | DOM}
+ */
+function selector (param) {
+      
+      if(param !== undefined) {
+
+            var d = document;
+            if(param[0] === "#") {
+                  return d.getElementById(param.substr(1, param.length - 1));
+            } else if (param[0] === "."){
+                  return d.getElementsByClassName(param.substr(1, param.length - 1));
+            }
+
+      } 
+}
+
+export {setDef, selector}
